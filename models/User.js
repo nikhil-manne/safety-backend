7f2330a0b075d513a959e6969200d109156f6bd2
nikhil-manne <163999262+nikhil-manne@users.noreply.github.com>
2025-09-06 16:12:01 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // (hashing recommended later)
+  password: { type: String, required: true },
+  mobile: { type: String, required: true },  // new
+  trustedContacts: [{ type: String }],       // new array for contacts
 });
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
+export default User;
